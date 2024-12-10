b324befb25cea181a8e5ff230e076796cd66afdc
Timote Viala <timote.viala@gmail.com>
2024-12-10 15:17:18 +0100

    Main 1.0.3

--- Main.py
+++ Main.py
@@ -15,18 +15,17 @@ class produit:
     
 
 
-# Affichage des produits 
+# Afficher le produit
 def afficher_produits(produits):
     if not produits:
         print("Produit introuvable")
     else:
         print(produits)
-    
 
-# Trier les fichiers
 
-# Gestion liste produit
-def lire_produits(fichier):
+
+# Lecture produits dans les fichiers
+def lecture_produits(fichier):
     produits = []
     if os.path.exists(fichier): # verification existance fichier
         with open(fichier, 'r') as f: #ouvir et fermer les fichiers
@@ -36,18 +35,80 @@ def lire_produits(fichier):
                     produits.append(produit(parts[0], parts[1], parts[2, parts[3]])) #verification ligne contien bien 4 valeurs
             produits
 
-# Recherche
+# Tri
+def triage_produits(produits, research):
+    if research == 'nom':
+        return 
+    elif research == 'prix':
+        return
+    elif research == 'quantite':
+        return
+    elif research == 'disponible':
+        return 
+    else:
+        return produits
+
+
+# Recherche 
+def rechercher_produit(produits, nom):
+    resultat = [p for p in produits if nom.lower() in p.nom.lower()]
+    return resultat
 
 # Ajouter/supprimer des produits
 def ajouter_produits(fichier, produit):
     with open(fichier, 'a') as f:  # 'a' permet de si fichier deja cree modifier la valeur 
         f.write(f"{produit.nom}, {produit.prix}, {produit.quantite}")
 
-# Menu interactif 
-while True : 
-     print("Menu")
-     print("Voir Liste Produits")
-     print("Trier produits")
-     print("Rechercher Produits")
-     print("Ajouter/supprimer des produits")
+# Exit 
+#def exit(produits):
+    
 
+
+
+# Menu interactif 
+def menu_interactif():
+    fichier = 'produits.txt'
+    produit = lecture_produits(fichier)
+    
+    while True : 
+        print("Menu")
+        print("1 - Voir Liste Produits")
+        print("2 - Trier produits")
+        print("3 - Rechercher Produits")
+        print("4 - Ajouter/supprimer des produits")
+        print("5 - Exit")
+
+        choix = input("Choisir : ")
+
+        if choix == 1 :
+            afficher_produits(produit)
+        
+        elif choix == 2:
+            research = input("Trie par Nom Prix Quantite Disponibilite :")
+            produits_tri = tri(produit, research)
+            triage_produits(produits_tri)
+        
+        elif choix == 3:
+            rechercher_produit = input("Produit a rechercher :")
+            recherche = rechercher_produit(produit, rechercher_produit)
+            rechercher_produit(recherche)
+        
+        elif choix == 4:
+           # ajouter_produits = input("Ajouter produit: ")
+           # ajout = ajouter_produits(produit)
+            nom = input("Nom :")
+            prix = input("Prix :")
+            quantite = input("Quantité :")
+            produit 
+            ajouter_produits(fichier, produit)
+            produits.append(produit)
+            print("Produit ajouter dans votre reserve")
+
+        elif == 5:
+            print("Exit")
+            break
+
+        else:
+            print("Valeur non valide veuillez recommencer: ")
+
+     
