9804cf332daa3b174900c27154d8c97de6b76505
Timote Viala <timote.viala@gmail.com>
2024-12-12 17:31:36 +0100

    A

--- utilisateur.py
+++ utilisateur.py
@@ -1,4 +1,5 @@
-
+import csv
+import os
 
 
 
@@ -6,8 +7,60 @@
 class utilisateur:
     def __init__(self, fichier_utilisateur='utilisateur.csv') :
         self.fichier_utilisateur = fichier_utilisateur
+    
 
+def creation_compte(self):
+        if not os.path.exists(self.fichier_utilisateurs):
 
+            with open(self.fichier_utilisateurs, 'w', newline='') as fichier:
+                writer = csv.writer(fichier)
+                writer.writerow(['nom_utilisateur', 'mot_de_passe', 'grade'])
+    
+def nom_utilisateur():
+    
+    
+def mot_de_passe():
+     
+     
+     
+     
+     
+     
+     
+def menu():
+    gestion = utilisateur()
 
-if __name__ == 
+    while True:
+        print("\n\nMenu")
+        print("1 - Creation compte")
+        print("2 - Se connecter")
+        print("3 - Changer de password")
+        print("4 - Supprimer le compte")
+        print("5 - Exit")
+
+        choix = int("Choisir un numero: ")
+
+        if choix == '1':
+            nom_utilisateur = input("Indiquer nom utilisateur: ")
+            mot_de_passe = input("indiquer mot de passe: ")
+            gestion.
+             
+        elif choix == '2': 
         
+        elif choix == '3':
+        
+        elif choix == '4':
+        
+        elif choix == '5':
+            print("Exit")
+            break
+
+        else:
+            print("Numero non referencier")
+    
+
+
+
+
+if __name__ == "":
+    x
